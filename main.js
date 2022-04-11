c937ab8d7eac94822da35d8c3e896351c61589b9
gisdevops-narendra <narendra.m@garudayltics.com>
2022-04-11 21:21:19 +0530

    added popup

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
+var content;
 var view = new ol.View({
     projection: 'EPSG:4326',
     center: [70.97667574234188, 20.716470556284726],
     zoom: 15.047395063939724
 });
-
 //here i am defining new control with extending openlayers default control
 //custom control for create point
 var button = document.createElement('button');
 button.innerHTML = '<i class="fas fa-pencil-ruler"></i>';
-button.id='createpointid';
-var handleRotateNorth = function() {
+button.id = 'createpointid';
+var handleRotateNorth = function () {
     startDrawing();
 };
 button.addEventListener('click', handleRotateNorth, false);
@@ -22,8 +22,8 @@ var createPointControl = new ol.control.Control({
 //custom control for legend showing
 var buttonlegend = document.createElement('button');
 buttonlegend.innerHTML = '<i class="fa-solid fa-image-portrait"></i>';
-buttonlegend.id='legendid';
-var legendfun = function() {
+buttonlegend.id = 'legendid';
+var legendfun = function () {
     slegend();
 };
 buttonlegend.addEventListener('click', legendfun, false);
@@ -36,8 +36,8 @@ var legendControl = new ol.control.Control({
 //custom control for open  database Table
 var buttonDbTable = document.createElement('button');
 buttonDbTable.innerHTML = '<i class="fa-solid fa-table-list"></i>';
-buttonDbTable.id='tableid';
-var table = function() {
+buttonDbTable.id = 'tableid';
+var table = function () {
     closeTble();
 };
 buttonDbTable.addEventListener('click', table, false);
@@ -47,14 +47,27 @@ element.appendChild(buttonDbTable);
 var tableControl = new ol.control.Control({
     element: element
 });
+//custom control for feature inforamtion 
+var buttonInformation = document.createElement('button');
+buttonInformation.innerHTML = '<i class="fa-solid fa-info"></i>';
+buttonInformation.id = 'infoid';
+var information = function () {
+    getinfo();
+};
+buttonInformation.addEventListener('click', information, false);
+var element = document.createElement('div');
+element.className = 'information-class ol-unselectable ol-control';
+element.appendChild(buttonInformation);
+var featureInfoarmtion = new ol.control.Control({
+    element: element
+});
 //here i am ending new control with extending openlayers default control
-
 var map = new ol.Map({
     target: 'map',
     view: view
 });
 var layerosm = new ol.layer.Tile({
-    title:"osm",
+    title: "osm",
     source: new ol.source.OSM()
 });
 var wmsKlTasjSource = new ol.source.ImageWMS({
@@ -66,70 +79,61 @@ var wmsKlTasjSource = new ol.source.ImageWMS({
 var wmsKlTaskLayer = new ol.layer.Image({
     visible: true,
     //extent: [71.46409117326702, 21.62483263358209, 74.73231625985055, 22.856996557147635],
-    title: "Surveyed Layer",
+    title: "database",
     source: wmsKlTasjSource
 });
 var layerSwitcher = new ol.control.LayerSwitcher({
- 
 });
 map.addControl(layerSwitcher);
 map.addLayer(layerosm);
 map.addLayer(wmsKlTaskLayer);
-
 //Adding extended contrl over here
 map.addControl(createPointControl);
 map.addControl(legendControl);
 map.addControl(tableControl);
-
+map.addControl(featureInfoarmtion);
+var popup = new Popup();
+map.addOverlay(popup);
 // get button id so on click we draw the point
 var startDraw = document.getElementById('icon');
-
 //create a vector sources so that use for vector data
 var vSource = new ol.source.Vector();
-
 //create vector layer for vector source
 var vLayer = new ol.layer.Vector({
     source: vSource
 });
 map.addLayer(vLayer);
-
 //here i have to firstly initalise the draw event
 var drawPoint = new ol.interaction.Draw({
     source: vSource,//above source vector source
     type: 'Point' //linestring,polygon
 });
-
 //clear draw on again draw start
 drawPoint.on('drawstart', function (e) {
     //debugger;
     vSource.clear();
     //alert("draw start");
 });
-
-
 $('.modal-dialog').draggable({
     handle: ".modal-header"
-  });
-
+});
 drawPoint.on('drawend', function (cordinate) {
     $("#myModal").modal('show');
-  
+
     // $("#myModal").show();
     debugger;
     pointCordainate = cordinate.feature.getGeometry().getFlatCoordinates();
-   
+
     //console.log(cordinate.feature.getGeometry().getFlatCoordinates());
     //alert(cordinate.feature.getGeometry().getFlatCoordinates())
     //alert(pointCordainate);
     //alert("draw end");
 });
 function startDrawing() {
-   // debugger;
+    // debugger;
     map.addInteraction(drawPoint);
 };
-
-
-function savedataintodb(){
+function savedataintodb() {
     //alert("submited btn clicked")
     var featuresname = document.getElementById('ename').value;
     var featuretype = document.getElementById('ddlpointtype').value;
@@ -137,62 +141,58 @@ function savedataintodb(){
     var ulat = pointCordainate[1];
     //console.log(featuresname,featuretype,ulat,ulong);
 
-    if(featuresname=='' || featuretype=='' || ulong=='' || ulat==''){
+    if (featuresname == '' || featuretype == '' || ulong == '' || ulat == '') {
         debugger;
         alert("please enter all details");
-    }else{
-    $.ajax(
-        {
-            url: 'data.php',
-            type: 'POST',
-            //dataType:'json',
-            data:
+    } else {
+        $.ajax(
             {
-                fname: featuresname,
-                ftype: featuretype,
-                longitude: ulong,
-                latitude: ulat
-            },
-            success: function (data) {
-                $("#myModal").modal('hide');
-                // $("#myModal").hide();
-                vSource.clear();
-                map.removeInteraction(drawPoint);
-                $("#featuresData").html(data);
-                $("#tble-data").css("display", "block");
-                $("#legend").css("display", "block");
-                $("#ename").val(''); 
-                $("#ddlpointtype").val(''); 
+                url: 'data.php',
+                type: 'POST',
+                //dataType:'json',
+                data:
+                {
+                    fname: featuresname,
+                    ftype: featuretype,
+                    longitude: ulong,
+                    latitude: ulat
+                },
+                success: function (data) {
+                    $("#myModal").modal('hide');
+                    // $("#myModal").hide();
+                    vSource.clear();
+                    map.removeInteraction(drawPoint);
+                    $("#featuresData").html(data);
+                    $("#tble-data").css("display", "block");
+                    $("#legend").css("display", "block");
+                    $("#ename").val('');
+                    $("#ddlpointtype").val('');
 
-                //alert(result);
+                    //alert(result);
+                }
             }
-        }
-    )
-};
+        )
+    };
 }
-function clearAddPoint(){
+function clearAddPoint() {
     map.removeInteraction(drawPoint);
     vSource.clear();
     $("#myModal").modal('hide');
     // $("#divaddpoint").hide();
 }
-
 const updateLegend = function (resolution) {
     const graphicUrl = wmsKlTasjSource.getLegendUrl(resolution);
     const img = document.getElementById('legend');
     img.src = graphicUrl;
-  };
-
-  // Initial legend
+};
+// Initial legend
 const resolution = map.getView().getResolution();
 updateLegend(resolution);
-
 // Update the legend when the resolution changes
 map.getView().on('change:resolution', function (event) {
-  const resolution = event.target.getResolution();
-  updateLegend(resolution);
+    const resolution = event.target.getResolution();
+    updateLegend(resolution);
 });
-
 function slegend() {
     var x = document.getElementById("legend");
     if (x.style.display == "block") {
@@ -201,7 +201,6 @@ function slegend() {
         x.style.display = "block";
     }
 }
-
 function closeTble() {
     var y = document.getElementById("tble-data");
     if (y.style.display == "block") {
@@ -210,5 +209,34 @@ function closeTble() {
         y.style.display = "block";
     }
 }
+function getinfo(evt) {
+    map.on('singleclick', function (evt) {
+        var viewResolution = (view.getResolution());
+        if (popup) {
+            popup.hide();
+        }
+        if (content) {
+            content = '';
+        }
+        map.getLayers().getArray().slice().forEach(layer => {
+            var layer_title = layer.get('title');
+            var wmsSource = new ol.source.ImageWMS({
+                url: 'http://localhost:8080/geoserver/wms',
+                params: {
+                    'LAYERS': layer_title
+                },
+                serverType: 'geoserver',
+                crossOrigin: 'anonymous'
+            });
+            var url = wmsSource.getFeatureInfoUrl(
+                evt.coordinate, viewResolution, 'EPSG:4326', {
+                'INFO_FORMAT': 'text/html'
+            });
+            $.get(url, function (data) {
+                popup.show(evt.coordinate, data);
+            });
 
+        });
+    })
 
+}
